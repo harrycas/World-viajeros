018d3faef393471250a3e9c67c92a4dfe6f81bdd
harrycas <hcastror1@upao.edu.pe>
2024-01-09 19:00:20 -0500

    Proyect Initializer

---
+++ src/main/java/com/personal/PP_viajeros/PpViajerosApplication.java
@@ -0,0 +1,13 @@
+package com.personal.PP_viajeros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpViajerosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpViajerosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/PP_viajeros/PpViajerosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.PP_viajeros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpViajerosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
